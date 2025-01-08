c04ef510966225f4fc5ecbaac5cd5dfb87edd260
qwsnxnjene <khalimov480@gmail.com>
2025-01-09 00:00:02 +0300

    third part

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/qwsnxnjene/go_final_project/db"
 	"github.com/qwsnxnjene/go_final_project/tests"
@@ -20,11 +21,29 @@ func startServer() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	http.HandleFunc("/api/nextdate", NextDateHandler)
 
 	err := http.ListenAndServe(ports, nil)
 	if err != nil {
 		log.Fatal(fmt.Errorf("can't start a server: %v", err))
 		return
+	}
+}
+
+func NextDateHandler(rw http.ResponseWriter, r *http.Request) {
+	date, repeat := r.FormValue("date"), r.FormValue("repeat")
+	now, err := time.Parse("20060102", r.FormValue("now"))
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	newDate, err := NextDate(now, date, repeat)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(err.Error()))
+	} else {
+		rw.Write([]byte(newDate))
 	}
 }
 
@@ -35,6 +54,7 @@ func createDB() {
 	}
 	_, err := os.Stat(dbPath)
 
+	// if there is no DB, create one
 	var install bool
 	if err != nil {
 		install = true
