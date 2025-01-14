0df4e3680faeb2d80e6e0cffa65c3595a6444ada
qwsnxnjene <khalimov480@gmail.com>
2025-01-14 11:23:51 +0300

    finalll

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,17 @@ COPY go.mod go.sum .env ./
 RUN go mod download
 
 COPY . .
+COPY db/scheduler.db /app/db/scheduler.db
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o go_final_project .
 
-FROM alpine:latest
+FROM ubuntu:latest
 
 WORKDIR /root/
 
 COPY --from=builder /app/go_final_project .
 COPY --from=builder /app/.env .
+COPY --from=builder /app/db/scheduler.db /root/db/scheduler.db
 
 EXPOSE 7540
 
--- main.go
+++ main.go
@@ -37,12 +37,13 @@ func startServer() {
 	http.HandleFunc("/api/tasks", authorization.Auth(handlers.TasksHandler))
 	http.HandleFunc("/api/task/done", authorization.Auth(handlers.TaskDoneHandler))
 	http.HandleFunc("/api/signin", handlers.SignInHandler)
-
+	fmt.Printf("Сервер запущен на порте %v\n", ports)
 	err = http.ListenAndServe(ports, nil)
 	if err != nil {
 		log.Fatal(fmt.Errorf("can't start a server: %v", err))
 		return
 	}
+
 }
 
 func main() {
